fa4ba2031f536513b4639bcbbdcc3163e4832c6b
Guilherme Rodrigues <guilherme.rodriguesm2301@gmail.com>
2025-03-19 17:13:46 -0300

    feat: type enum

---
+++ src/19basicTypes/typeEnum.ts
@@ -0,0 +1,25 @@
+//enum é uma estrutura não ordenada, de chave e valor, utilizada para quando se tem mais de uma opção pra alguma coisa
+enum Colors {
+  RED, // 0
+  BLUE, // 1
+  YELLOW, // 2
+}
+
+//quando há dois enums com mesmo nome ele faz a junção em uma única estrutura
+enum Colors {
+  GREEN = 'green',
+  PURPLE = 'purple',
+  BROWN = 5,
+  BLACK, // 6
+}
+
+console.log(Colors);
+
+console.log(Colors[0]);
+console.log(Colors.RED);
+
+function chooseColor(color: Colors): void {
+  console.log(color);
+}
+
+chooseColor(Colors.GREEN);
